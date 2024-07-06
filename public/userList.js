4335c0c820427b64125939a81c713b3022a1a644
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-06 22:58:47 +0600

    Redux Installed

---
+++ public/userList.js
@@ -0,0 +1,28 @@
+export const userList = [
+    { 
+        id: 1, 
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        email: 'jane.smith@example.com'
+    },
+    {
+        id: 3,
+        name: 'Mike Johnson',
+        email: 'mike.johnson@example.com'
+    },
+    {
+        id: 4,
+        name: 'Sarah Brown',
+        email:'sarah.brown@example.com'
+    },
+    {
+        id: 5,
+        name: 'David Wilson',
+        email: 'david.wilson@example.com'
+    }
+]
+        
