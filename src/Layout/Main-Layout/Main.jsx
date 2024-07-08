e42cb7d322679e8789790416bedf726bc8063c54
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-08 21:25:00 +0600

    Form Added

--- src/Layout/Main-Layout/Main.jsx
+++ src/Layout/Main-Layout/Main.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
     return (
-        <div className="container min-h-screen px-4 mx-auto">
+        <div className="container min-h-screen mx-auto bg-[#031b23] text-white">
             <Outlet></Outlet>
         </div>
     );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,20 @@
 
 const Home = () => {
 
+    let handleAddTodo = (e) =>{
+        e.priventDefault();
+        console.log(e);
+    }
+
     return (
-        <div>
-            <p>Hi! I am Home </p>
+        <div className="w-[800px] h-[600px] mx-auto">
+            <h1 className="text-4xl text-center font-bold pt-10">My Todo App</h1>
+            <div>
+                <form onSubmit={()=> handleAddTodo()} className="flex gap-5 py-5">
+                    <input type="text" className="border-none outline-none rounded-md text-black border-gray-400 py-1 flex-1 placeholder:pl-1" placeholder="Add a new todo..." />
+                    <button type="submit" className="bg-blue-500 text-white px-3 py-1 rounded-md">+</button>
+                </form>
+            </div>
         </div>
     );
 };
